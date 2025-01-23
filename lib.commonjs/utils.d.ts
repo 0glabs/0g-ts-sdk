134ffa039bfccf66b7d1639b9369f67fde86158e
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2025-01-23 14:37:39 -0500

    Update utils.d.ts

--- lib.commonjs/utils.d.ts
+++ lib.commonjs/utils.d.ts
@@ -1,5 +1,5 @@
-import { Signer } from 'ethers';
-import { ContractRunner } from 'ethers';
+import pkg from 'ethers';
+const { Signer, ContractRunner } = pkg;
 export declare function getFlowContract(address: string, signer: Signer): import("./contracts/flow/FixedPriceFlow.js").FixedPriceFlow;
 export declare function getMarketContract(address: string, runner: ContractRunner): import("./contracts/market/FixedPrice.js").FixedPrice;
 export declare function checkExist(inputPath: string): boolean;
